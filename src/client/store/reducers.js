4d0f7e1df0896d7686f254bfa55206cb221f3725
Tycho Bokdam <t.bokdam@seronaris.nl>
2017-03-21 08:53:07 +0100

    Update dependencies

--- src/client/store/reducers.js
+++ src/client/store/reducers.js
@@ -1,23 +1,15 @@
 import { combineReducers } from 'redux'
-import { routerReducer as router } from 'react-router-redux'
 
 // Fix: "React-Redux: Combining reducers: Unexpected Keys"
 // http://stackoverflow.com/a/33678198/789076
-const initialReducers = {
-}
+const initialReducers = {}
 
 export const makeRootReducer = (asyncReducers) => {
   return combineReducers({
     // Add sync reducers here
-    router,
     ...initialReducers,
     ...asyncReducers
   })
-}
-
-export const injectReducer = (store, { key, reducer }) => {
-  store.asyncReducers[key] = reducer
-  store.replaceReducer(makeRootReducer(store.asyncReducers))
 }
 
 export default makeRootReducer
