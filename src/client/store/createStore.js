0cff3b1752fcf23ca5020f626ca8e76099987a20
Tycho Bokdam <t.bokdam@seronaris.nl>
2017-03-23 18:48:27 +0100

    Fix for createLogger

--- src/client/store/createStore.js
+++ src/client/store/createStore.js
@@ -17,7 +17,7 @@ export default (initialState = {}, history, config) => {
   // Check if the logger middleware is enabled in the config
   if (middlewares.logger && middlewares.logger.enabled) {
     const loggerOptions = middlewares.logger.options || {}
-    const createLogger  = require('redux-logger').default
+    const createLogger  = require('redux-logger')
     const logger        = createLogger(loggerOptions)
 
     middleware.push(logger)
