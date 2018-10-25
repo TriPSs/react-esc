7ca20eb5ba50c8d3ee7796f902b4f86569197d08
Tycho Bokdam <t.bokdam@seronaris.nl>
2018-10-25 18:29:41 +0200

    feat(webpack-image): Added webpack-image package

--- packages/react-esc-webpack-image/index.js
+++ packages/react-esc-webpack-image/index.js
@@ -1,3 +1,38 @@
 module.exports = (config, options) => {
-  return {}
+  return {
+    module: {
+      rules: [
+        {
+          test   : /\.(gif|png|jpe?g|svg)$/i,
+          loaders: [
+            'file-loader?hash=sha512&digest=hex&name=img/img-[name]-[hash:6].[ext]',
+            {
+              loader: 'image-webpack-loader',
+              query : {
+                pngquant: {
+                  optimizationLevel: 7,
+                  quality          : '65-90',
+                  speed            : 4,
+                },
+
+                bypassOnDebug: true,
+
+                mozjpeg: {
+                  progressive: true,
+                },
+
+                optipng: {
+                  optimizationLevel: 7,
+                },
+
+                gifsicle: {
+                  interlaced: false,
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+  }
 }
