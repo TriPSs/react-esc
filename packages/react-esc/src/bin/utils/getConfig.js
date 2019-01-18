19008a4f7f6bc0c83786d110c4fdd1041ac3f0d7
Tycho Bokdam <t.bokdam@seronaris.nl>
2019-01-18 17:14:57 +0100

    fix: Don't let rollup output something when creating the config

--- packages/react-esc/src/bin/utils/getConfig.js
+++ packages/react-esc/src/bin/utils/getConfig.js
@@ -1,13 +1,23 @@
 import fs from 'fs'
 import path from 'path'
 import { execSync } from 'child_process'
+import debug from 'debug'
+
+const log = debug('react-esc:cli:config')
 
 export default (cwd, cli) => {
   const configLocation = path.resolve(cwd, cli.config || '.esc-config.js')
 
   if (fs.existsSync(configLocation)) {
+    log('Creating...')
+
     const configLoc = path.resolve(__dirname, '../../config.js')
-    execSync(`rollup ${configLocation} --file ${configLoc} --format cjs -c ${path.resolve(__dirname, '../../rollup.config.js')}`)
+    execSync(
+      `rollup ${configLocation} --file ${configLoc} --format cjs -c ${path.resolve(__dirname, '../../rollup.config.js')}`,
+      {
+        stdio: 'ignore',
+      },
+    )
 
     return require(configLoc)
   }
